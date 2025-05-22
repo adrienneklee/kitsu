39f4fba9cffe962a809cd4fdc0dcc55e784be606
“ADRIENNE <adrienneklee@gmail.com>
2025-05-22 10:33:53 -0400

    updated logo

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { Menu, X } from 'lucide-react';
-import Logo from '/kitsu_logo.png';
+import Logo from '/kitsu_logo_gold_transparent.png';
 
 export default function Navbar() {
   const location = useLocation();
@@ -16,12 +16,12 @@ export default function Navbar() {
   ];
 
   return (
-    <nav className="fixed top-0 left-0 w-full bg-black bg-opacity-80 text-white z-50 px-8 py-4">
+    <nav className="fixed top-0 left-0 w-full bg-opacity-80 z-50 px-10 py-4">
       <div className="flex items-center justify-between">
         <img
           src={Logo}
           alt="Logo"
-          className="h-10 w-10 object-cover rounded-full"
+          className="h-35 w-70 object-cover"
         />
 
         <button
@@ -32,7 +32,7 @@ export default function Navbar() {
           {isOpen ? <X className="h-6 w-6" /> : <Menu className="h-6 w-6" />}
         </button>
 
-        <ul className="hidden md:flex space-x-6 text-sm tracking-widest uppercase">
+        <ul className="hidden md:flex space-x-6 text-m tracking-widest lowercase">
           {links.map(link => (
             <li key={link.path}>
               <Link
@@ -49,7 +49,7 @@ export default function Navbar() {
       </div>
 
       {isOpen && (
-        <ul className="flex flex-col space-y-4 mt-4 text-sm tracking-widest uppercase md:hidden">
+        <ul className="flex flex-col space-y-4 mt-4 text-m tracking-widest lowercase md:hidden">
           {links.map(link => (
             <li key={link.path}>
               <Link
